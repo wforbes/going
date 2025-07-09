fc904a574413f83663903d9f48bdbcb1b768e8e3
Will Forbes <wforbes87@gmail.com>
2025-07-09 13:59:06 -0700

    boot.dev - Learn Go lessons 1-8

--- README.md
+++ README.md
@@ -20,3 +20,28 @@ To build it we run `go build cmd/tutorial_1/main.go`, that produces a `main.exe`
 We can run that using just the exe's name `./main`.
 
 We can also, instead of building and running the exe, simply use the `go run` command to compile and run our project with that one command `go run cmd/tutorial_1/main.go`.
+
+## "Learn Go" course by Boot.dev (from Back-end Developer Path)
+
+[https://www.boot.dev/tracks/backend-python-golang](https://www.boot.dev/tracks/backend-python-golang)
+
+After installing and setting things up I checked out this course and just started appending it's exercises to the main function in 'cmd\tutorial_1'. The first section in the course is for variables, so I'm going to get through that and probably restructure the project. Especially when I learn how to create external modules or like... something to organize methods into (does Go have classes?). With that I'd be putting different lessons into their own files and probably having a little i/o looping routine to access them at runtime.
+
+### Notes: 
+
+Using the ['fmt' package](https://pkg.go.dev/fmt) to print text to the terminal. I'm impressed by the verbosity of the golang documentation. I wish I had infinite time to browse through the [standard library docs](https://pkg.go.dev/std).
+
+Go is a compiled language and it generally compiles much faster than other languages like Rust, Zig and C ... but it's execution performance is slower than them. By virtue of being a compile language it's execution performance is still faster than interpreted languages like python, javascript and ruby.
+
+``String access``: Getting the first character of a string isn't as simple as other langs. It looks like using the array accessor like someStr[0] actually accesses the byte at that location. So that means it has to be cast back to a string if you're going to do string stuff with that afterward, since the result is a byte.
+
+``Optional parameters``: Looks like Go doesn't allow you to use optional parameters in functions, so from what I'm seeing online a common solution is to either overload the function or to create a struct that defines a set of properties that may be passed in as options.
+
+``Random Links``
+- [func params](https://www.w3schools.com/go/go_function_parameters.php)
+- [accessing first char in str](https://www.bacancytechnology.com/qanda/golang/idiomatic-go-equivalent-of-cs-ternary-operator)
+- [accessing first char in str (reddit)](https://www.reddit.com/r/golang/comments/krxahr/how_do_i_make_optional_function_parameter_in_go/)
+- [accessing first char in str (article)](https://tipseason.com/how-to-get-first-character-in-string-golang/)
+- [learning about interfaces](https://go.dev/doc/effective_go#interfaces)
+
+['Which Type Should I Use?'](https://www.boot.dev/lessons/98e60d90-0111-4626-a690-70124be1e0ba) : Basically just use the default Go types unless you're really concerned about performance. 
--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -4,4 +4,150 @@ import "fmt"
 
 func main() {
 	fmt.Println("Going: the act of learning to code with Go")
+	bootDevLessons()
+}
+
+func bootDevLessons() {
+
+	printSectionStart("Chapter 1: Variables", true)
+	bootDevC1L2()
+	bootDevC1L3()
+	bootDevC1L4()
+	/*
+		boot.dev Chapter 1, Lesson 5
+		This is a multi-line comment,
+		same as it's always been
+		(https://www.boot.dev/lessons/a581676a-054c-4c8f-a95d-396d626b0803)
+	*/
+
+	/*
+		boot.dev Chapter 1, Lesson 6
+		Talked about Compilation, the parts of the basic hello world file
+		- package main lets the Go compiler know that we want this code to compile
+			and run as a standalone program, as opposed to being a library that's
+			imported by other programs
+		- import "fmt" imports the formatting package from the standard library
+		- func main() defines the main function, the entry point for the program
+	*/
+
+	// boot.dev CH1L7 - Go compiles faster than other compiled languages, but runs slower
+	//	than others like Rust, C, Zig. Though since its a compiled language it runs faster
+	//	than interpreted languages like python, js, and ruby.
+	bootDevC1L8()
+
+	printSectionEnd(true)
+}
+
+func bootDevC1L2() {
+	printSectionStart("Chapter 1: Lesson 2", false)
+	// initializing variables with zero values
+	// (https://www.boot.dev/lessons/a1eae01c-0a40-47d5-9b98-94fe48199073)
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
+	fmt.Println("Initializing with zero values:")
+	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+}
+
+func bootDevC1L3() {
+	printSectionStart("Chapter 1: Lesson 3", false)
+	// using the walrus operator to declare new variables and assign values
+	// (can't be used outside of function scope)
+	// (https://www.boot.dev/lessons/62881ae0-89f4-44e6-b69e-50a7652a7da3)
+	messageStart := "Happy birthday! You are now"
+	age := 21
+	messageEnd := "years old!"
+	fmt.Println(messageStart, age, messageEnd)
+	printSectionEnd(false)
+}
+
+func bootDevC1L4() {
+	printSectionStart("Chapter 1: Lesson 4", false)
+	// casting a variable to a different type and using it in an expression
+	// (https://www.boot.dev/lessons/73145333-7245-4643-ae6b-e65a5f719906)
+	numMessagesFromDoris := 72
+	costPerMessage := .02
+	totalCost := costPerMessage * float64(numMessagesFromDoris)
+	fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
+	printSectionEnd(false)
+}
+
+func bootDevC1L8() {
+	printSectionStart("Chapter 1: Lesson 8", false)
+	// whole numbers
+	//	int int8 int16 int32 int64
+
+	// positive whole numbers (unsigned integers)
+	//	uint uint8 uint16 uint32 uint64 uintptr
+
+	// signed decimal numbers
+	//	float32 float64
+
+	// complex numbers (with real and imaginary part)
+	//	complex64 complex128
+
+	// the sizes (8, 16, 32, 64, 128) are how many bits in memory will be used to store the variable
+	// the default int and uint types refer to 32 or 64 depending on the users environment
+	// Use the standard sizes unless you have a performance requirement
+	//	int, uint, float64, complex128
+
+	// Some types can be easily converted:
+	temperatureFloat := 88.26
+	temperatureInt := int64(temperatureFloat)
+	fmt.Println("Converted from float:(", temperatureFloat, ") to int:(", temperatureInt, ")")
+	// Casting a float to an int just truncates off the floating point part
+
+	accountAgeFloat := 2.6
+	accountAgeInt := int64(accountAgeFloat)
+	fmt.Println("Your account has existed for", accountAgeInt, "years")
+	printSectionEnd(false)
+}
+
+func Ternary(condition bool, ifTrue any, ifFalse any) any {
+	if condition {
+		return ifTrue
+	}
+	return ifFalse
+}
+
+func printSectionStart(displayTxt string, thick bool) {
+	seperator, ok := Ternary(thick, "=", "-").(string) // assert that string is returned from Ternary
+	if !ok {                                           // ok is a boolean that will be false if assertion failed
+		fmt.Println(displayTxt)
+		return
+	}
+	fmt.Println()
+	printSeperator(seperator)
+	fmt.Println(displayTxt)
+	printSeperator(seperator)
+	fmt.Println()
+}
+
+func printSectionEnd(thick bool) {
+	seperator, ok := Ternary(thick, "=", "-").(string) // assert that string is returned from Ternary
+	if !ok {                                           // ok is a boolean that will be false if assertion failed
+		return
+	}
+	fmt.Println()
+	printSeperator(seperator)
+	fmt.Println()
+}
+
+func printSeperator(char string) {
+	if len(char) == 0 {
+		char = "-"
+	}
+	if len(char) > 1 {
+		// get first character of the string and set that as the 'char' var
+		char = string(char[0]) // access first byte, then convert it to string?
+	}
+	width := 36
+	var output string
+	// could also use range like:
+	// for range width {}
+	for i := 0; i < width; i++ {
+		output += char
+	}
+	fmt.Println(output)
 }
