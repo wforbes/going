6e4cf35873e0862f0f94af356ad663cf4e13cfd3
Will Forbes <wforbes87@gmail.com>
2025-07-09 21:39:56 -0700

    add boot.dev Learn Go lesson 4 and 5

--- internal/bootdev/learnGo/chapter2.go
+++ internal/bootdev/learnGo/chapter2.go
@@ -8,6 +8,9 @@ import (
 func runChapter2Lessons() {
 	chapter2Lesson1()
 	chapter2Lesson2()
+	// lesson 3 is just a question related to lesson 2
+	chapter2Lesson4()
+	chapter2lesson5()
 }
 
 func chapter2Lesson1() {
@@ -39,6 +42,64 @@ func chapter2Lesson2() {
 	} else {
 		fmt.Println("Message is within the limit")
 	}
+
+	utils.PrintSectionEnd(false)
+}
+
+func chapter2Lesson4() {
+	utils.PrintSectionStart("Chapter 2: Lesson 4 - Switch", false)
+	//(https://www.boot.dev/lessons/f8b2fcea-078b-41be-b59b-8bece5ae923b)
+	plan := "basic"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "pro"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "enterprise"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "free"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "unknown"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+
+	utils.PrintSectionEnd(false)
+}
+
+func billingCost(plan string) float64 {
+	switch plan {
+	case "basic":
+		return 10.00
+	case "pro":
+		return 20.0
+	case "enterprise":
+		return 50.0
+	default:
+		return 0.0
+	}
+}
+
+func chapter2lesson5() {
+	utils.PrintSectionStart("Chapter 2: Lesson 5 - Calculate Balance", false)
+	//(https://www.boot.dev/lessons/f6c53b4a-3bfe-49ca-8733-aa15c19fe7b1)
+
+	var insufficientFundMessage string = "Purchase failed. Insufficient funds."
+	var purchaseSuccessMessage string = "Purchase successful."
+	var accountBalance float64 = 100.0
+	var bulkMessageCost float64 = 75.0
+	var isPremiumUser bool = true
+	var discountRate float64 = 0.10
+	var finalCost float64
+
+	finalCost = bulkMessageCost
+	if isPremiumUser {
+		finalCost = bulkMessageCost * (1 - discountRate)
+	}
+	if finalCost <= accountBalance {
+		accountBalance -= finalCost
+		fmt.Println(purchaseSuccessMessage)
+	} else {
+		fmt.Println(insufficientFundMessage)
+	}
+
+	fmt.Println("Account balance:", accountBalance)
 
 	utils.PrintSectionEnd(false)
 }
