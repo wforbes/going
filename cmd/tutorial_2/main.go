fe547778fc92c1cf2e3fa3aa6385525452e65b9f
Will Forbes <wforbes87@gmail.com>
2025-07-14 20:49:23 -0700

    add Learn GO Fast video code from ts 9:30-13:13

--- cmd/tutorial_2/main.go
+++ cmd/tutorial_2/main.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 	"unsafe"
 )
 
@@ -49,4 +50,57 @@ func main() {
 	//	you've got to cast the data types to match
 	var result float32 = floatNum32 + float32(intNum32)
 	fmt.Println(floatNum32, "+", intNum32, "=", result)
+
+	// VIDEO TIMESTAMP 9:35 ~
+
+	var intNum1 int = 3
+	var intNum2 int = 2
+	fmt.Println(intNum1 / intNum2) //integer division rounds down
+	fmt.Println(intNum1 % intNum2) //modulous operator works as expected to get remainder
+
+	var myString string = "Strings: You can use double quotes, but you can't line wrap them without the \\n newline char"
+	fmt.Println(myString)
+	var myString2 string = `Strings: You can use the backtick symbol
+to make it a "raw string literal" and it will
+conform to any whitespace within the bounds of the backticks`
+	fmt.Println(myString2)
+	var myString3 string = "Strings: You" + " " + " can concatinate them " + "with the + operator!"
+	fmt.Println(myString3)
+
+	// you can get the length of a string with len(s) but this can be decieving
+	//	because its the number of bytes... not the number of characters in the str
+	fmt.Println("Length of the string \"test\": ", len("test")) // returns 4
+	// GO uses utf-8 encoding, chars outside of the normal 256 ascii charset are stored in
+	//	more than a single byte
+	fmt.Println("Length of thinking-face emoji: ", len("🤔"))  // returns 4
+	fmt.Println("Length of double-daggar symbol: ", len("‡")) // returns 3
+	// So to get the true length we can import the "unicode/utf8" package and work with Runes
+	fmt.Println("Rune count of double-daggar symbol: ", utf8.RuneCountInString("‡")) // returns 1
+
+	//A rune is an alias for int32 and represents a Unicode code point.
+	// Since emojis are part of the Unicode standard, they can be directly assigned to rune variables.
+	var myRune rune = '😏'
+	fmt.Printf("For emojis we can format strings to output them like this: %c \n", myRune)
+
+	var myBoolean bool = false
+	fmt.Println("Boolean types: ", myBoolean)
+
+	// Go sets uninitialized primatives as a default value
+	//	Strings is ""
+	//	All numbers is 0
+	//	Booleans is false
+
+	var myVar = "This type is inferred"
+	fmt.Println("Declaring a variable without a type allows type inference: ", myVar)
+	myVar2 := "This type is inferred too"
+	fmt.Println("We can even drop the 'var' and use the := walrus/shorthand symbol to declare an inferred variable: ", myVar2)
+	var1, var2 := 1, 2
+	fmt.Printf("We can declare multiple inferred type vars of the same type at the same with commas... %v %v\n", var1, var2)
+
+	const myConst string = "const value"
+	fmt.Println("Constants cant be changed, must be declared with a value: ", myConst)
+
+	// VIDEO TIMESTAMP 13:13 ~
+	// Continued in tutorial_3 folder of this project
+
 }
