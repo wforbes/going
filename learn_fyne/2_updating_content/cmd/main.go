4a865e126f52fe038208e918c4244a25ddcb11f3
Will Forbes <wforbes87@gmail.com>
2025-07-22 15:11:38 -0700

    fyne: mess around n add a button

--- learn_fyne/2_updating_content/cmd/main.go
+++ learn_fyne/2_updating_content/cmd/main.go
@@ -1,17 +1,23 @@
 // Fyne Getting Started: Updating Content
 // https://www.youtube.com/watch?v=h2ZOdTA3ew4
+// update: found that marking a moment in time with a button click
+//
+//	seemed to be causing a rounding up second because the UI updates
+//	seconds approx .28s after the actual channel tick
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func updateTime(clock *widget.Label) {
-	formatted := time.Now().Format("The time is 03:04:05")
+	formatted := time.Now().Format("The time is 03:04:05.000")
 	clock.SetText(formatted)
 }
 
@@ -21,14 +27,24 @@ func main() {
 
 	clock := widget.NewLabel("")
 	updateTime(clock)
-	w.SetContent(clock)
+
+	markedTimeLabel := widget.NewLabel("Marked = --:--:--.---")
+	button := widget.NewButton("Mark Time", func() {
+		formattedTime := time.Now().Format("Marked = 03:04:05.000")
+		fmt.Println(formattedTime)
+		markedTimeLabel.SetText(formattedTime)
+	})
+	w.SetContent(container.NewVBox(
+		clock,
+		markedTimeLabel,
+		button,
+	))
 	go func() {
 		ticker := time.NewTicker(time.Second)
 		for range ticker.C {
 			fyne.Do(func() {
 				updateTime(clock)
 			})
-
 		}
 	}()
 
